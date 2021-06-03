f7e0d044fd11e4ba2def1fdb3b1c2f1f41618116
hariharandev2020 <hariharan29220gmail.com>
2021-06-03 15:34:30 +0530

    Remove alert

--- Calendar/js/calender.js
+++ Calendar/js/calender.js
@@ -5,15 +5,11 @@ function gettingDay(){
     var month = ["January","February","March","April","May","June","July","Auguest","September","October","November","December"];
     var monthDays = [31,28,31,30,31,30,31,31,30,31,30,31];
     var day  = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
     var monthVal = document.querySelector("#month").value -1;    
-    // alert(monthVal);
     var date = document.querySelector("#date").value;
     var year = 2021;
-
-
     var  m = monthDays[monthVal];
-//     alert(m);
+
     document.querySelector("#ans-3").style.color = "white";
 
  
@@ -40,7 +36,6 @@ function gettingDay(){
      
     
     var nDay = new Date(year,monthVal,date).getDay();
-    // alert(nDay);
     
      if ( (nDay == 0) || (nDay == 6) ) {
         
