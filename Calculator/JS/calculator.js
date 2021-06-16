d1268eb6b256c41efc16eefeed28f3631f2ac35e
hariharandev2020 <hariharan29220gmail.com>
2021-06-16 17:18:49 +0530

    Calculator design added Successfully

--- Calculator/JS/calculator.js
+++ Calculator/JS/calculator.js
@@ -1,64 +1,63 @@
-    
-    var show      = document.getElementById('displaycontent');
-    var container = document.querySelector('#container');
-    var newValu   = document.querySelectorAll(".number").textContent;
+      let show             = document.querySelector("#ans");
+      let preview          = document.querySelector("#anss");
+      let result;   
 
-    
-     
-    var x;
-    var y;  
-    var displayValue;
-    var result;
-    
- 	function darkTheme(){
-    
-      show.style.color ="black";
-      show.style.border = "5px solid  rgb(40, 42, 44)"
-      container.style.backgroundColor="rgb(40, 42, 44)";
-      container.style.transition="2s";
-      show.style.backgroundColor="white";
-     
-    }
- 	function lightTheme(){
-    
-      show.style.backgroundColor ="#2DBBB8";
-      show.style.border = "none";
-      container.style.backgroundColor="white";
-      container.style.transition="2s"; 
-            
- 	}
+    function number(x){
 
- 	function clr(){
- 	 
-      show.innerHTML=" ";      
- 
+            preview.innerHTML = '';
+            preview.innerHTML += x;
+            show.innerHTML += x;    
+     }
+
+    function operator(y){
+
+      let check = /[+,*,/,%,-]/g;
+
+            if( (show.innerHTML === '' ) || preview.innerHTML === ''){
+
+                  alert("Please enter a number ( 1 - 10 ) first");
+                  return false;
+            }
+            else if(y === '=' ){
+
+                  if( show.innerText.match(check) ){
+
+                        preview.innerHTML = '';
+                        result = eval(show.innerText);
+                        preview.innerHTML = result;
+                        show.innerHTML    += y; 
+
+                  }else {
+
+                        alert("Please enter any operator (+, -, *, %, / )")
+                        return false;       
+                  }     
+
+                  } else{
+
+                        preview.innerHTML = '';
+                        preview.innerHTML += y;
+                        show.innerHTML    += y;      
+                  }      
+       
+            }
+
+
+    function clr(){   
+       
+       show.innerHTML    = '';
+       preview.classList.toggle("ani");
+       preview.innerHTML = 0; 
+        
  	}   
 
-    function mul(){
- 	
-    	show.innerHTML= "*";
- 	
-  	}
+//     function animation(){
 
-   function dev(){
+      
 
-    	show.innerHTML= "%";
- 	
- 	    
-  }
+//     }
+//     function stopAnimate(){
 
-    function plus(){
- 	
-     show.innerHTML = " + ";
-     var x = show.textContent + newValu.textContent;
-     show.innerHTML = x;
-    
-     
- 	}
+//       preview.style.animationPlayState = "paused";
 
-   function min(){
- 	
-     show.innerHTML ="-"; 	
- 	 
-     
- 	}
+//     }
